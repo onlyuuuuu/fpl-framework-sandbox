be6da8b58a7ce4be0097a833f64f7a26c53f81e0
Andy <duyanhnn@outlook.com>
2023-08-24 16:23:12 +0700

    First commit for playgrounds

---
+++ databases/mysql/off.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker compose --project-name fpl-sandbox down
+# To remove volume
+#docker compose down --volume
---
+++ databases/mysql/on.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose --project-name fpl-sandbox up -d
